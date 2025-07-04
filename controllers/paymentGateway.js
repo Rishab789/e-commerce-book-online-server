c279ab48ae84e41155c8c52f60becfee71227e58
Rishab789 <beraprakash456@gmail.com>
2025-07-05 03:55:25 +0530

    modified payment controller

--- controllers/paymentGateway.js
+++ controllers/paymentGateway.js
@@ -1,47 +1,57 @@
 const crypto = require("crypto");
-const { Cashfree } = require("cashfree-pg");
-const { request } = require("http");
+const { Cashfree, CashfreePG, CFEnvironment } = require("cashfree-pg");
 
-Cashfree.XClientId = process.env.CLIENT_ID;
-Cashfree.XClientSecret = process.env.CLIENT_SECRET;
-Cashfree.XEnvironment = Cashfree.Environment.SANDBOX;
+// Create config instance
+const cfConfig = new Cashfree.CFConfig(
+  CFEnvironment.SANDBOX,
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET
+);
 
+// Initialize PG client
+const pg = new CashfreePG(cfConfig);
+
+// Generate a random order ID
 function getOrderId() {
   const uniqueId = crypto.randomBytes(16).toString("hex");
-
   const hash = crypto.createHash("sha256");
   hash.update(uniqueId);
-
   const orderId = hash.digest("hex");
-
-  return orderId.substr(0, 12);
+  return orderId.substr(0, 12); // Return 12-char order ID
 }
 
+// Payment handler
 exports.payment = async (req, res) => {
   try {
-    let request = {
-      order_amount: "1",
+    const request = {
+      order_amount: 1,
       order_currency: "INR",
-      order_id: await getOrderId(),
+      order_id: getOrderId(),
       customer_details: {
         customer_id: "node_sdk_test",
-        customer_name: "",
+        customer_name: "John Doe",
         customer_email: "example@gmail.com",
         customer_phone: "9999999999",
       },
+      order_meta: {
+        return_url:
+          "https://test.cashfree.com/pgappsdemos/return.php?order_id=order_123",
+      },
     };
-    Cashfree.PGCreateOrder("05-07-2025", request)
-      .then((response) => {
-        console.log(response.data);
-        res.json(response.data);
-      })
-      .catch((err) => {
-        console.log(err.response.data.message);
-      });
-  } catch (err) {}
+
+    const response = await pg.orders.createOrder(request);
+    res.json(response.data);
+  } catch (err) {
+    console.error("Order creation error:", err?.response?.data || err.message);
+    res.status(500).send("Payment creation failed.");
+  }
 };
 
+// Verification handler (empty for now)
 exports.verify = async (req, res) => {
   try {
-  } catch (err) {}
+    res.send("Verify logic to be implemented");
+  } catch (err) {
+    res.status(500).send("Verification failed.");
+  }
 };
